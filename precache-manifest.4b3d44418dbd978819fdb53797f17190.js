d1d0236452d950b50846f8d68ba77b0883183ec4
Delvaux <Robby>
2020-10-14 22:09:35 +0200

    Updates

---
+++ precache-manifest.4b3d44418dbd978819fdb53797f17190.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2344ca1f0929a24fdc8d856b80598fe9",
+    "url": "/react-pomodoro/index.html"
+  },
+  {
+    "revision": "32cc20184f8a6fc9fde4",
+    "url": "/react-pomodoro/static/css/2.6414bb7b.chunk.css"
+  },
+  {
+    "revision": "34079acf0c43cacd3644",
+    "url": "/react-pomodoro/static/css/main.a61fad29.chunk.css"
+  },
+  {
+    "revision": "32cc20184f8a6fc9fde4",
+    "url": "/react-pomodoro/static/js/2.bb530e63.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-pomodoro/static/js/2.bb530e63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34079acf0c43cacd3644",
+    "url": "/react-pomodoro/static/js/main.53839cef.chunk.js"
+  },
+  {
+    "revision": "7bccaee7c4903395c012",
+    "url": "/react-pomodoro/static/js/runtime-main.44319084.js"
+  }
+]);
